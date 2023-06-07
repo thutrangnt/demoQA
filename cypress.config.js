1a68d92944175e32b30efe0267df271baf6c3834
thutrangnt <thutrang18680@gmail.com>
2023-06-07 09:57:53 +0700

    Add report Awesome

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
