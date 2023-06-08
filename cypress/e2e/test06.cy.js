340d440eb83c7f841d6c3570a21df4bfa0a29eea
thutrangnt <thutrang18680@gmail.com>
2023-06-08 09:19:52 +0700

    Report new

--- cypress/e2e/test06.cy.js
+++ cypress/e2e/test06.cy.js
@@ -1,15 +1,15 @@
 
-const { homePage } = require("../support/pageObject/homePage")
-const { loginPage } = require("../support/pageObject/loginPage")
-const { printBarcodePage } = require("../support/pageObject/printBarcodePage")
-describe('template spec', () => {
-  beforeEach('Open app', () => {
-      cy.visit('https://sma.tecdiary.net/admin/login')
-  })
-  it('TC06: Verify Print Barcode/Label page', ()=>{
-    loginPage.loginToHomePage('admin@tecdiary.com','12345678')
-    homePage.goToPrintBarCode()
-    printBarcodePage.verifyPrintBarcodeElement()
+// const { homePage } = require("../support/pageObject/homePage")
+// const { loginPage } = require("../support/pageObject/loginPage")
+// const { printBarcodePage } = require("../support/pageObject/printBarcodePage")
+// describe('template spec', () => {
+//   beforeEach('Open app', () => {
+//       cy.visit('https://sma.tecdiary.net/admin/login')
+//   })
+//   it('TC06: Verify Print Barcode/Label page', ()=>{
+//     loginPage.loginToHomePage('admin@tecdiary.com','12345678')
+//     homePage.goToPrintBarCode()
+//     printBarcodePage.verifyPrintBarcodeElement()
     
-  })
-})
+//   })
+// })
